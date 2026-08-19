09a4b4a8bcbb50a4e49ecee9d42e93470fa6a8d1
Harshal <harshal17290@gmail.com>
2026-08-19 22:07:22 +0530

    Switch public port to Streamlit

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 uvicorn delivery_api:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit frontend in the foreground
-streamlit run app.py --server.port=8501 --server.address=0.0.0.0
+streamlit run app.py --server.$port=8501 --server.address=0.0.0.0
